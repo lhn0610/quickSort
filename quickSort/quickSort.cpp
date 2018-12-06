7470f70f0387f56955bd1683ea8d8026347eef93
lhn0610 <2212980862@qq.com>
2018-12-06 14:39:26 +0800

    Add project files.

---
+++ quickSort/quickSort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int partition(int arr[], int startIndex, int endIndex) {
+	int pivot = arr[startIndex];
+	int left = startIndex;
+	int right = endIndex;
+	int index = startIndex;
+
+	while (right >= left) {
+		while (right >= left) {
+			if (arr[right] < pivot) {
+				arr[left] = arr[right];
+				index = right;
+				left++;
+				break;
+			}
+			right--;
+		}
+
+		while (right >= left) {
+			if (arr[left] > pivot) {
+				arr[right] = arr[left];
+				index = left;
+				right--;
+				break;
+			}
+			left++;
+		}
+	}
+	arr[index] = pivot;
+	return index;
+}
+void quickSort(int arr[], int startIndex, int endIndex)
+{
+	if (startIndex >= endIndex) {
+		return;
+	}
+	int pivotIndex = partition(arr, startIndex, endIndex);
+	quickSort(arr, startIndex, pivotIndex - 1);
+	quickSort(arr, pivotIndex + 1, endIndex);
+}
+
+int main() {
+	int arr[8] = { 1, 2, 3, 4, 5, 6, 1, 8 };
+	quickSort(arr, 0, 7);
+	for (int i = 0; i < 8; i++) {
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+	return 0;
+}
